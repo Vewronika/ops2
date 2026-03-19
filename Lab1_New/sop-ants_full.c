7d2515b6ed6e9b9682e9baa5248312b58175b86d
Vewronika <veronika2801k@gmail.com>
2026-03-19 11:15:09 +0100

    sop-ants

--- Lab1_New/sop-ants_full.c
+++ Lab1_New/sop-ants_full.c
@@ -1,3 +1,159 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ERR(source) \
+    (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+#define MAX_GRAPH_NODES 32
+#define MAX_PATH_LENGTH (2 * MAX_GRAPH_NODES)
+
+#define FIFO_NAME "/tmp/colony_fifo"
+
+volatile sig_atomic_t stop_work;
+static int read_fd;
+
+typedef struct node
+{
+    int indexes[MAX_GRAPH_NODES];
+    int neighbours_num;
+    int pipe[2];
+}node_t;
+
+
+typedef struct graph
+{  
+    node_t nodes[MAX_GRAPH_NODES];
+    int node_num;
+}graph_t;
+
+typedef struct ant
+{
+    int ID;
+    int path[MAX_PATH_LENGTH];
+    int path_length;
+}ant_t;
+
+
+int set_handler(void (*f)(int), int sig)
+{
+    struct sigaction act = {0};
+    act.sa_handler = f;
+    if (sigaction(sig, &act, NULL) == -1)
+        return -1;
+    return 0;
+}
+
+void sig_handler(int signal){
+    if(signal == SIGINT){
+        stop_work = 1;
+        close(read_fd);
+    }
+}
+
+void msleep(int ms)
+{
+    struct timespec tt;
+    tt.tv_sec = ms / 1000;
+    tt.tv_nsec = (ms % 1000) * 1000000;
+    while (nanosleep(&tt, &tt) == -1)
+    {
+    }
+}
+
+void usage(int argc, char* argv[])
+{
+    printf("%s graph start dest\n", argv[0]);
+    printf("  graph - path to file containing colony graph\n");
+    printf("  start - starting node index\n");
+    printf("  dest - destination node index\n");
+    exit(EXIT_FAILURE);
+}
+
+graph_t read_colony(char* name){
+    FILE* fl = fopen(name, "r");
+    if(fl == NULL){
+        ERR("fopen");
+    }
+
+    graph_t graph;
+    if(fscanf(fl, "%d", &graph.node_num) != 1){
+        ERR("fscanf");
+    }
+
+    while(1){
+        int from;
+        int to;
+        if(fscanf(fl, "%d %d", &from, &to) != 2){
+            break;
+        }  
+        node_t* node_ptr = &graph.nodes[from];
+        node_ptr->indexes[node_ptr->neighbours_num++] = to;
+    }
+    fclose(fl);
+    return graph;
+}
+
+void child_work(node_t node, int index, int fd_w[MAX_GRAPH_NODES], int destination_idx){
+    srand(getpid());
+    set_handler(sig_handler, SIGINT);
+    printf("{%d}: ", index);
+    for(int i = 0; i < node.neighbours_num; i++){
+        printf("%d ", node.indexes[i]);
+    }
+    printf("\n");
+    read_fd = node.pipe[0];
+    int fd_r = open(FIFO_NAME, O_WRONLY);
+    while(!stop_work){
+        ant_t ant;
+        if(read(node.pipe[0], &ant, sizeof(ant)) < 0){
+            if(errno == EINTR || errno == EBADF){
+                break;
+            }
+            ERR("read");
+        }
+        msleep(100);
+        ant.path[ant.path_length++] = index;
+        if(node.neighbours_num == 0 || ant.path_length == MAX_PATH_LENGTH){
+            printf("Ant {%d}: got lost\n", ant.ID);
+        }
+        else if(index == destination_idx){
+            printf("Ant {%d}: found food\n", ant.ID);
+            if(write(fd_r, &ant, sizeof(ant)) == -1){
+                ERR("write");
+            }
+        }
+        else{
+            int r = rand()%node.neighbours_num;
+            if(write(fd_w[node.indexes[r]], &ant, sizeof(ant)) == -1){
+                if(errno == EINTR){
+                    break;
+                }
+                if(errno == EPIPE){
+                    printf("Ant {%d}: got lost\n", ant.ID);
+                    continue;
+                }
+                fprintf(stderr, "%d\n", r);
+                ERR("write");
+            }
+            if(rand()%50 == 0){
+                printf("Node {%d}: collapsed\n", index);
+                break;
+            }
+        }
+    }
+    close(fd_r);
+}
+
 
 
 int main(int argc, char* argv[])
